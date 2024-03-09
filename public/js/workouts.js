f86f243562918e3c2c04755c2aec45855c59b5e9
lyp1noff <lyp1noff@gmail.com>
2024-03-09 18:47:09 +0000

    Proto of workout page and draggable list

---
+++ public/js/workouts.js
@@ -0,0 +1,32 @@
+import './index.js'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const addWorkoutBtn = document.querySelector('#addWorkout');
+    addWorkoutBtn.addEventListener('click', () => {
+        showContent("addWorkoutMenu");
+    });
+})
+
+const sortableList = document.querySelector(".sortable-list");
+const items = sortableList.querySelectorAll(".item");
+
+items.forEach(item => {
+    item.addEventListener("dragstart", () => {
+        setTimeout(() => item.classList.add("dragging"), 0);
+    });
+    item.addEventListener("dragend", () => item.classList.remove("dragging"));
+});
+
+const initSortableList = (e) => {
+    e.preventDefault();
+    const draggingItem = document.querySelector(".dragging");
+    let siblings = [...sortableList.querySelectorAll(".item:not(.dragging)")];
+    let nextSibling = siblings.find(sibling => {
+        return e.clientY <= sibling.offsetTop + sibling.offsetHeight / 2;
+    });
+
+    sortableList.insertBefore(draggingItem, nextSibling);
+}
+
+sortableList.addEventListener("dragover", initSortableList);
+sortableList.addEventListener("dragenter", e => e.preventDefault());
